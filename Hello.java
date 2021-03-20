ccc3611d428cd36f8cd98b84f5c34a444bdd656c
ketan kotian <43913711+ketankotian@users.noreply.github.com>
2021-03-20 09:40:55 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 public String toString
 public static void main(String [] args){
-	System.out.println("Hello world !");
+	System.out.println("Hi ");
+		System.out.println("Hello world !");
 }
 }
